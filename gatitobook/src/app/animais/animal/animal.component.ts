91ac54247975280d311441775b449ba323d17532
Edvan.ximes <endvan@gmail.com>
2022-08-09 23:01:13 -0300

    Revert "LoginGuard" iniciando gatibook versao 2

---
+++ gatitobook/src/app/animais/animal/animal.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {environment} from 'src/environments/environment';
+
+const API = environment.apiURL;
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css'],
+})
+export class AnimalComponent implements OnInit {
+  @Input() descricao = '';
+  private urlOriginal = '';
+
+  constructor() {}
+
+  get url(): string {
+    return this.urlOriginal;
+  }
+
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.urlOriginal = url;
+    } else {
+      this.urlOriginal = `${API}/imgs/${url}`;
+    }
+  }
+
+  ngOnInit(): void {}
+}
