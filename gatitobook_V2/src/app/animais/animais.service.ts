7eae169d4cb18d2ce9602daf886ebf015b6532fe
Edvan.ximes <endvan@gmail.com>
2022-08-14 11:25:44 -0300

    Métodos de curtir e excluir foto

--- gatitobook_V2/src/app/animais/animais.service.ts
+++ gatitobook_V2/src/app/animais/animais.service.ts
@@ -1,16 +1,19 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
 import {environment} from 'src/environments/environment';
 import {TokenService} from '../autenticacao/token.service';
 import {Animais, Animal} from './animais';
+import {catchError, mapTo} from 'rxjs/operators';
 
 const API = environment.apiURL;
+const NOT_MODIFILD = '304';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimaisService {
+
   constructor(private http: HttpClient, private tokenService: TokenService) {
   }
 
@@ -21,4 +24,20 @@ export class AnimaisService {
   buscaPorId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
   }
+
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http.post(`${API}/photos/${id}/likes`, {}, {observe: 'response'})
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFILD ? of(false) : throwError(error);
+        })
+      );
+  }
+
+
 }
