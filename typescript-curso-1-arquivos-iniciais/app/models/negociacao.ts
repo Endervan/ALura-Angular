dc4d06040563b9c02ac835c16405dd575955f363
Edvan.ximes <endvan@gmail.com>
2022-10-21 23:04:22 -0300

    reestrutura Javascript pra typescript ES6

---
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -0,0 +1,32 @@
+export class Negociacao {
+    // # => atributos privados
+    #data;
+    #quantidade;
+    #valor;
+
+    constructor(data, quantidade, valor) {
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+        this.#data = data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
+    }
+
+
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get volume (){
+        return this.#quantidade * this.#valor;
+    }
+}
