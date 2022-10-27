2d1bd9c805643628ca03d10ac161ff759250ada5
Edvan.ximes <endvan@gmail.com>
2022-10-26 23:52:03 -0300

    Métodos privados

--- typescript-curso-1-arquivos-iniciais/dist/js/views/negociacoes-views.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/views/negociacoes-views.js
@@ -14,7 +14,7 @@ export class NegociacoesViews extends View {
             ${model.lista().map(negociacao => {
             return `
                 <tr>
-                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                    <td>${this.formatar(negociacao.data)}</td>
                     <td>${negociacao.quantidade}</td>
                     <td>${negociacao.valor}</td>
                 </tr>
@@ -23,5 +23,8 @@ export class NegociacoesViews extends View {
        </tbody>
         </table>
         `;
+    }
+    formatar(data) {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
