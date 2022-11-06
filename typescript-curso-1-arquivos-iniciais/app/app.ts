7fa77b67baa3fd373120ac391c297eb24035ff0f
Ender <endvan@gmail.com>
2022-11-06 12:42:14 -0300

    Tratando null em nosso código

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -3,10 +3,15 @@ import {NegociacaoController} from "./controllers/negociacao-controller.js";
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-})
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    })
+} else {
+    throw Error("Não foi possivel inicializaar a aplicação.verifique se o form e null")
+}
+
 
 
 
