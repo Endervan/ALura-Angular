2c305028fd37349b974f72d0bb8d0dceb05aea22
Ender <endvan@gmail.com>
2022-11-06 12:38:04 -0300

    Suprimindo a checagem onde faz sentido

--- typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
+++ typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
@@ -14,9 +14,10 @@ export class NegociacaoController {
 
 
     constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        // this.inputData = document.querySelector('#data') as HTMLInputElement; == linha abaixo
+        this.inputData = <HTMLInputElement>document.querySelector('#data') ;
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
 
     }
