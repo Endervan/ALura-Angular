624e6c3278ad77eedd18449d09cfac6f10b7f830
Ender <endvan@gmail.com>
2022-11-06 00:38:16 -0300

    Protegendo nosso template

--- typescript-curso-1-arquivos-iniciais/app/views/view.ts
+++ typescript-curso-1-arquivos-iniciais/app/views/view.ts
@@ -1,14 +1,20 @@
 export abstract class View<T> {
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor)
+    constructor(seletor: string,escapar?:boolean) {
+        this.elemento = document.querySelector(seletor);
+
     }
 
 
     update(model: T): void {
         // converte string em element DOM
-        this.elemento.innerHTML = this.template(model);
+        let template = this.template(model);
+        if (this.escapar){ // removendo script malicioso template
+            template = template.replace(/<scrip>[\s\S]*?<\/script>/,'');
+        }
+        this.elemento.innerHTML = template;
     }
 
     // metodo ‘abstract’ forca filho a sobrescreve o metodo
