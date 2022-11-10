8ca49fcce3fd2c252733b04174c0b118c42e4c7b
Ender <endvan@gmail.com>
2022-11-10 20:20:18 -0300

    Operador Map

--- alura_angular_rxjs_1-master/byte-bank-broker/src/app/acoes/acoes.service.ts
+++ alura_angular_rxjs_1-master/byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Acao} from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,22 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getAcoes(){
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+  getAcoes() {
+    // map altera fluxo informaçes apresentadas
+    // sort ordenacao array
+    return this.httpClient.get<any>('http://localhost:3000/acoes').pipe(
+      map((acoes) => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB)))
+    );
+  }
+
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    } else if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    } else {
+      return 0;
+    }
   }
 }
