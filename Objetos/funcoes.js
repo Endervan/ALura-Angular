4e4ac06faba30f859b38652f249b1bc8234a09d9
Edvan.ximes <endvan@gmail.com>
2023-02-26 12:20:44 -0300

    Funções dentro 0bjeto

---
+++ Objetos/funcoes.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+    saldo: 200,
+    efetuaPagamento:function (valor) {
+        if (valor > this.saldo){
+            console.log("Saldo insufuciente")
+        }else {
+            this.saldo -= valor;
+            console.log(`Pagamento realizado . novo saldo : ${this.saldo}`)
+        }
+    }
+};
+
+
+cliente.efetuaPagamento(190);
+
