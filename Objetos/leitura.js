93a0b60866ceb82c4291c3c4def5439ab2c87e86
Edvan.ximes <endvan@gmail.com>
2023-03-01 23:56:20 -0300

    Operações com um JSON

--- Objetos/leitura.js
+++ Objetos/leitura.js
@@ -1,4 +1,12 @@
 const  dados = require("./cliente.json");
 
 console.log(dados)
-console.log(typeof dados)
+console.log(typeof dados);
+const clienteEmString = JSON.stringify(dados); // transforma obj em string
+
+console.log(clienteEmString);
+console.log(typeof clienteEmString)
+
+const objCliente = JSON.parse(clienteEmString);
+console.log(objCliente)
+console.log( typeof objCliente)
