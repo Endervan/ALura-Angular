3598ed60402ab1c4f5815077adaf7823ec8c69a9
Edvan.ximes <endvan@gmail.com>
2023-03-02 00:46:30 -0300

    Ordenando com reverse

--- Objetos/ordernar.js
+++ Objetos/ordernar.js
@@ -1,8 +1,8 @@
 const clientes = require("./clientes.json");
 
 
-function ordernar(lista, propriedade) {
-    return lista.sort((a, b) => {
+function ordernar(lista, propriedade, ordem = "crescente") {
+    const resultado = lista.sort((a, b) => {
         if (a[propriedade] > b[propriedade]) {
             return 1;
         } else if (a[propriedade] < b[propriedade]) {
@@ -11,9 +11,14 @@ function ordernar(lista, propriedade) {
             return 0
         }
     });
+    if (ordem !== "crescente") {
+        resultado.reverse();
+    }
+    return resultado;
 }
 
-const ordenaPeloNome = ordernar(clientes,"nome");
+const ordenaPeloNome = ordernar(clientes, "nome");
+// const ordenadoInverso = ordenaPeloNome.reverse();
 
 console.log(ordenaPeloNome)
 
