aecec239b2f649b547a21d49b11108d8e4cacbb9
Edvan.ximes <endvan@gmail.com>
2023-03-17 21:13:22 -0300

    Aplicando cache decorator copy

--- typescript-curso-3/app/dist/js/decorators/dom-Injector.js
+++ typescript-curso-3/app/dist/js/decorators/dom-Injector.js
@@ -2,10 +2,14 @@ export function domInjector(seletor) {
     return function (target, propertyKey) {
         console.log(`Modificando Prototype ${target.constructor.name} 
         e adicionando get para a propriedade ${propertyKey}`);
+        let elemento;
         const getter = function () {
-            console.log(`Buscando elemento do Dom  com o selector ${seletor}
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`Buscando elemento do Dom  com o selector ${seletor}
              para injetar em ${propertyKey}`);
-            return document.querySelector(seletor);
+            }
+            return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
     };
--- typescript-curso-3/app/src/decorators/dom-Injector.ts
+++ typescript-curso-3/app/src/decorators/dom-Injector.ts
@@ -2,11 +2,17 @@ export function domInjector(seletor: string) {
     // target => propriedade estatica de classe retorna funcao construtora
     return function (target: any, propertyKey: string) {
         console.log(`Modificando Prototype ${target.constructor.name} 
-        e adicionando get para a propriedade ${propertyKey}`)
+        e adicionando get para a propriedade ${propertyKey}`);
+
+        let elemento: HTMLElement; // cast elementos
         const getter = function () {
-            console.log(`Buscando elemento do Dom  com o selector ${seletor}
+            if (!elemento) { // elemento com tipo HTMLInputElement nunca sera nulo
+                elemento = <HTMLInputElement>document.querySelector(seletor);
+                console.log(`Buscando elemento do Dom  com o selector ${seletor}
              para injetar em ${propertyKey}`)
-            return document.querySelector(seletor)
+            }
+
+            return elemento
         }
 
         Object.defineProperty(
