cf4f7d527c908a098d79dbfcda7a5097d9a5f5ec
Edvan.ximes <endvan@gmail.com>
2023-06-07 12:11:59 -0300

    component criado

--- React/orgs-cesta/App.js
+++ React/orgs-cesta/App.js
@@ -1,20 +1,21 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import Cestas from "./telas/Cestas";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start wgasdfjgajorking on your app111!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Cestas></Cestas>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
