c9f1380ec178f93462bfd0a5daaf1ebd2e333561
Edvan.ximes <endvan@gmail.com>
2023-06-10 21:34:31 -0300

    resume hooks

--- React/react-native-utilizando-e-criando-hooks/src/telas/Home/componentes/Produtor.js
+++ React/react-native-utilizando-e-criando-hooks/src/telas/Home/componentes/Produtor.js
@@ -82,3 +82,20 @@ const estilos = StyleSheet.create({
     }
 
 });
+
+
+// Hooks Básicos:
+//
+//     useState: usado para gerenciar estados;
+// useEffect: usado para gerar efeitos colaterais a certas atualizações;
+// useContext: usado para gerenciar contextos entre componentes distintos.
+
+//     Hooks Adicionais:
+//
+// useReducer: uma alternativa ao useState quando há lógicas complexas;
+// useCallback: memoriza uma função, que só é atualizada quando os valores recebidos como argumento são atualizados;
+// useMemo: semelhante ao useCallback, mas ao invés de função, armazena um valor;
+// useRef: usado normalmente para acessar propriedades de componentes “filhos”;
+// useImperativeHandle: usado para personalizar o valor da instância que será acessível aos componentes “pais”;
+// useLayoutEffect: semelhante ao useEffect, porém é disparado na mesma fase que componentDidMount e componentDidUpdate. É preferível o uso do useEffect;
+// useDebugValue: usado para definir um "label" em hooks customizados que irá aparecer somente em ferramentas de debug.
