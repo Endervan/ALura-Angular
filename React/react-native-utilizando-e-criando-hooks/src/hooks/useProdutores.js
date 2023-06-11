60a84a034c5fd16ae0784dd9c8cbaff0b1736592
Edvan.ximes <endvan@gmail.com>
2023-06-11 10:36:50 -0300

    ordenacao

--- React/react-native-utilizando-e-criando-hooks/src/hooks/useProdutores.js
+++ React/react-native-utilizando-e-criando-hooks/src/hooks/useProdutores.js
@@ -6,12 +6,19 @@ export default function useProdutores() {
     const [titulo, setTitulo] = useState('');
     const [lista, setLista] = useState([]);
 
+    function compareDistancia(produtor1, produtor2) {
+        return produtor1.distancia - produtor2.distancia;
+    }
+
     useEffect(() => {
         const retorno = carregaProdutores();
+        const listaOrdenada = retorno.lista.sort(compareDistancia);
+
+
         setTitulo(retorno.titulo);
-        setLista(retorno.lista)
-    }, []);//array vazio fica parecindo com life ciclo componentDidMount qnd usa classe
+        setLista(listaOrdenada)
+    }, []);//array vazio fica parecido com life ciclo componentDidMount qnd usa classe
 
 
-    return [titulo,lista];
+    return [titulo, lista];
 }
