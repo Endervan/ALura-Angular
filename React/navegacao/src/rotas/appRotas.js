9f71b8713a157dfc579e0b9c33bb37fa1a22e7e7
Edvan.ximes <endvan@gmail.com>
2023-06-12 10:47:58 -0300

    Navegando para o tela (Stack)

--- React/navegacao/src/rotas/appRotas.js
+++ React/navegacao/src/rotas/appRotas.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Home from "../telas/Home";
-import MelhoresProdutores from "../telas/MelhoresProdutores";
+import ProdutorRotas from "./ProdutorRotas";
+import MelhoresProdutoresRotas from "./MelhoresProdutoresRotas";
 
 const Tab = createBottomTabNavigator();
 
 
 export default function AppRotas() {
- return <NavigationContainer>
+    return <NavigationContainer>
         <Tab.Navigator>
-            <Tab.Screen name='Home' component={Home} />
-            <Tab.Screen name='Melhores Produtores' component={MelhoresProdutores} />
+            <Tab.Screen name='Home' component={ProdutorRotas}/>
+            <Tab.Screen name='Melhores Produtores' component={MelhoresProdutoresRotas}/>
         </Tab.Navigator>
     </NavigationContainer>
 
--- React/navegacao/src/telas/Produtores/index.js
+++ React/navegacao/src/telas/Produtores/index.js
@@ -5,38 +5,45 @@ import Produtor from './componentes/Produtor';
 import Topo from './componentes/Topo';
 import useProdutores from '../../hooks/useProdutores';
 import useTextos from '../../hooks/useTextos';
+import {useNavigation} from "@react-navigation/native";
 
-export default function Produtores({ melhoresProdutores }) {
-  const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
+export default function Produtores({melhoresProdutores}) {
+    const navigation = useNavigation();
 
-  const TopoLista = () => {
-    return <>
-      <Topo melhoresProdutores={melhoresProdutores} />
-      <Text style={estilos.titulo}>{tituloProdutores}</Text>
-    </>
-  }
+    const lista = useProdutores(melhoresProdutores);
+    const {tituloProdutores} = useTextos();
 
-  return <FlatList
-    data={lista}
-    renderItem={
-      ({ item }) => <Produtor {...item} aoPressionar={() => {}} />
+    const TopoLista = () => {
+        return <>
+            <Topo melhoresProdutores={melhoresProdutores}/>
+            <Text style={estilos.titulo}>{tituloProdutores}</Text>
+        </>
     }
-    keyExtractor={({ nome }) => nome}
-    ListHeaderComponent={TopoLista}
-    style={estilos.lista} />
+
+    return <FlatList
+        data={lista}
+        renderItem={
+            ({item}) => <Produtor
+                {...item}
+                aoPressionar={() => {
+                    navigation.navigate('Produtor')
+                }}/>
+        }
+        keyExtractor={({nome}) => nome}
+        ListHeaderComponent={TopoLista}
+        style={estilos.lista}/>
 }
 
 const estilos = StyleSheet.create({
-  lista: {
-    backgroundColor: '#ffffff',
-  },
-  titulo: {
-    fontSize: 20,
-    lineHeight: 32,
-    marginHorizontal: 16,
-    marginTop: 16,
-    fontWeight: 'bold',
-    color: '#464646',
-  }
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    titulo: {
+        fontSize: 20,
+        lineHeight: 32,
+        marginHorizontal: 16,
+        marginTop: 16,
+        fontWeight: 'bold',
+        color: '#464646',
+    }
 })
