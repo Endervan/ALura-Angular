ae9e1c3f75d2d3146ce0d3797573d08823b0461e
endvan Ximenes <endvan@gmail.com>
2023-06-12 11:59:58 -0300

    Estrutura do Produtor

--- React/navegacao/src/componentes/Topo.js
+++ React/navegacao/src/componentes/Topo.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import {Dimensions, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {Dimensions, Image, StyleSheet} from 'react-native';
 
 import Texto from './Texto';
-
-import Gradiente from '../assets/gradiente.svg';
 import topo from '../assets/topo.png';
-import VoltarSVG from '../assets/voltar.svg';
 
 const largura = Dimensions.get('screen').width;
 const ALTURA_PADRAO = 270;
@@ -13,14 +10,14 @@ const ALTURA_PADRAO = 270;
 export default function Topo({ titulo, imagem = topo, altura = ALTURA_PADRAO }) {
   const estilos = funcaoEstilos(altura);
   return <>
-    <Image source={imagem} style={estilos.topo} />
-    <Gradiente width={largura} height={130 / 360 * largura} style={estilos.gradiente}/>
-    <Texto style={estilos.titulo}>{ titulo }</Texto>
-    <TouchableOpacity
-      onPress={() => {}}
-      style={estilos.botaoVoltar}>
-      <VoltarSVG color='white' style={estilos.voltar} />
-    </TouchableOpacity>
+    <Image source={imagem} style={estilos.topo}/>
+    {/*<Gradiente width={largura} height={130 / 360 * largura} style={estilos.gradiente}/>*/}
+    <Texto style={estilos.titulo}>{titulo}</Texto>
+    {/*<TouchableOpacity*/}
+    {/*  onPress={() => {}}*/}
+    {/*  style={estilos.botaoVoltar}>*/}
+    {/*  <VoltarSVG color='white' style={estilos.voltar} />*/}
+    {/*</TouchableOpacity>*/}
   </>
 }
 
--- React/navegacao/src/telas/Produtor/index.js
+++ React/navegacao/src/telas/Produtor/index.js
@@ -1,6 +1,69 @@
 import React from "react";
-import {Text} from "react-native";
+import {useRoute} from "@react-navigation/native";
+import {FlatList, Image, StyleSheet, Text, View} from "react-native";
+import Cesta from "./componentes/Cesta";
+import Topo from "../../componentes/Topo";
+import useTextos from "../../hooks/useTextos";
+
+import topo from '../../assets/produtores/topo.png'
 
 export default function () {
-    return <Text>Detalhes Produtor</Text>
+    const route = useRoute();
+    const {tituloProdutor, tituloCestas} = useTextos();
+    const {nome, imagem, cestas} = route.params;
+
+    const TopoLista = () => {
+        return <>
+            <Topo titulo={tituloProdutor} imagem={topo} altura={150}/>
+            <View style={estilos.conteudo}>
+                <View style={estilos.logo}>
+                    <Image source={imagem} style={estilos.produtorImage}/>
+                    <Text style={estilos.produtor}>{nome}</Text>
+                </View>
+                <Text style={estilos.cestas}>{tituloCestas}</Text>
+            </View>
+        </>
+    }
+
+    return <FlatList
+        ListHeaderComponent={TopoLista}
+        data={cestas}
+        renderItem={({item}) => <Cesta {...item} produtor={{nome, imagem}}/>}
+        style={estilos.lista}
+        keyExtractor={item => item}
+
+    />
 }
+
+
+const estilos = StyleSheet.create({
+    lista: {
+        backgroundColor: '#ffffff',
+    },
+    conteudo: {
+        paddingHorizontal: 16,
+    },
+    logo: {
+        flexDirection: 'row',
+    },
+    produtorImage: {
+        width: 62,
+        height: 62,
+        marginTop: -23,
+        borderRadius: 6,
+    },
+    produtor: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginLeft: 16,
+    },
+    cestas: {
+        color: '#464646',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        marginTop: 32,
+    }
+});
