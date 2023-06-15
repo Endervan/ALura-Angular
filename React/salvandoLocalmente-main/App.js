a874fb4821834c4f747f2717c30bb746f9a24e05
Edvan.ximes <endvan@gmail.com>
2023-06-15 10:09:20 -0300

    Mostrando as notas(storage)

--- React/salvandoLocalmente-main/App.js
+++ React/salvandoLocalmente-main/App.js
@@ -1,7 +1,8 @@
-import {SafeAreaView, StatusBar, StyleSheet} from "react-native"
+import {FlatList, SafeAreaView, StatusBar, StyleSheet} from "react-native"
 import NotaEditor from "./src/componentes/NotaEditor";
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import {useState} from "react";
+import {Nota} from "./src/componentes/Nota";
 
 
 export default function App() {
@@ -20,6 +21,10 @@ export default function App() {
 
     return (
         <SafeAreaView style={estilos.container}>
+            <FlatList data={notas}
+                      renderItem={(nota)=><Nota {...nota}/>}
+                      keyExtractor={nota=>nota[0]}
+            />
             <NotaEditor mostraNotas={mostraNotas}/>
             <StatusBar/>
         </SafeAreaView>
