8ea3b0b332bc3120b7302c50282ba1529e6e2eda
Edvan.ximes <endvan@gmail.com>
2023-06-26 17:35:17 -0300

    Manipulando o Componente test

--- React/react-native-criando-testes-para-sua-aplicacao-main/__tests/telas/Leilao/componentes/enviaLance.test.js
+++ React/react-native-criando-testes-para-sua-aplicacao-main/__tests/telas/Leilao/componentes/enviaLance.test.js
@@ -1,24 +1,65 @@
-import React from "react";
-import {render} from "@testing-library/react-native";
-import EnviaLance from "../../../../src/telas/Leilao/componentes/EnviaLance";
-import {ENVIADO} from "../../../../src/negocio/constantes/estadosLance";
+import React from 'react';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import {ENVIADO, NAO_ENVIADO} from '../../../../src/negocio/constantes/estadosLance';
 
+import EnviaLance from '../../../../src/telas/Leilao/componentes/EnviaLance';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 describe('telas/Leilao/componentes/EnviaLance', () => {
-    it('deve enviar lanche quando for enviado', () => {
+    it('deve enviar o lance quando o botão for pressionado', async () => {
+        const enviaLance = jest.fn(() => new Promise(resolve => resolve(ENVIADO)))
 
-        const enviaLance = jest.fn(()=> new Promise((resolve)=> resolve(ENVIADO)));
-
-        const {toJSON} = render(
+        const {
+            getByPlaceholderText,
+            getByA11yHint,
+            getByText
+        } = render(
             <EnviaLance
-                enviaLance={() => enviaLance}
+                enviaLance={enviaLance}
                 cor="blue"
             />
         );
-        console.log(toJSON());
+
+        const input = getByPlaceholderText("R$");
+        const botao = getByA11yHint("Enviar lance");
+
+        fireEvent.changeText(input, "10");
+        fireEvent.press(botao);
+
+        expect(enviaLance).toHaveBeenCalledWith("10");
+        await waitFor(() => {
+            expect(getByText(ENVIADO)).toBeTruthy();
+        });
+
+        expect(() => getByText(NAO_ENVIADO)).toThrow();
     });
 
+    it('deve exibir o erro quando o lance não for enviado', async () => {
+        const enviaLance = jest.fn(() => new Promise(resolve => resolve(NAO_ENVIADO)))
 
+        const {
+            getByPlaceholderText,
+            getByA11yHint,
+            getByText
+        } = render(
+            <EnviaLance
+                enviaLance={enviaLance}
+                cor="blue"
+            />
+        );
 
+        const input = getByPlaceholderText("R$");
+        const botao = getByA11yHint("Enviar lance");
 
-})
+        fireEvent.changeText(input, "10");
+        fireEvent.press(botao);
+
+        expect(enviaLance).toHaveBeenCalledWith("10");
+        await waitFor(() => {
+            expect(getByText(NAO_ENVIADO)).toBeTruthy();
+        });
+
+        expect(() => getByText(ENVIADO)).toThrow();
+    });
+});
