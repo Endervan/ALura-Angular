55a775de909370378a2de5c01c51ac3389157326
Edvan.ximes <endvan@gmail.com>
2023-06-27 16:25:31 -0300

    Animando o carrossel (somente react)

--- React/react-native-animacoes-projeto-base/src/componentes/Carrocel/index.js
+++ React/react-native-animacoes-projeto-base/src/componentes/Carrocel/index.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {FlatList, Image, View} from "react-native";
 import styles from "./styles";
 
 
 export function Carrocel({data}) {
+    const carrosselRef = useRef();
+    const [indice, setIndice] = useState(0);
+
+    useEffect(() => {
+        carrosselRef.current.scrollToIndex({index: indice});
+        const intervalo = setInterval(() => {
+            alterarPosicaoObjeto();
+        }, 2000)
+        return () => clearInterval(intervalo);
+    }, [indice]);
+
+    function alterarPosicaoObjeto() {
+        if (indice < data.length - 1) {
+            setIndice(indice + 1);
+        } else {
+            setIndice(0)
+        }
+    }
+
     return (
         <View style={styles.conteiner}>
             <FlatList
@@ -11,13 +30,14 @@ export function Carrocel({data}) {
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 keyExtractor={item => item.id}
-                renderItem={({item}) => (
+                renderItem={({item, index}) => (
                     <Image
                         source={item.imagem}
-                        style={styles.image}
+                        style={[styles.image, index == data.length - 1 ? {marginRight: 200} : null]}
                         resizeMode="contain"
                     />
                 )}
+                ref={carrosselRef}
             />
         </View>
     )
