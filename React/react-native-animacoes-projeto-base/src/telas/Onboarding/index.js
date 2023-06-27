f6feecd593a54c35f9ef7a83d4b1f3aa5b7c8800
Ender <endvan@gmail.com>
2023-06-27 17:39:45 -0300

    Criando nosso Shimmer Effect(direto react)

--- React/react-native-animacoes-projeto-base/src/telas/Onboarding/index.js
+++ React/react-native-animacoes-projeto-base/src/telas/Onboarding/index.js
@@ -99,3 +99,6 @@ export default function Onboarding({navigation}) {
         </TelaDeFundo>
     );
 }
+
+
+// Shimmer Effect => carregamento esqueleto
--- React/react-native-animacoes-projeto-base/src/telas/Principal/index.js
+++ React/react-native-animacoes-projeto-base/src/telas/Principal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {FlatList, Text, TouchableOpacity, View} from "react-native";
 import {InformacoesUsuario} from "../../componentes/InformacoesUsuario";
 import {CardConsulta} from "../../componentes/CardConsulta";
@@ -6,31 +6,52 @@ import {TelaDeFundo} from "../../componentes/TelaDeFundo";
 import soniaFoto from "../../assets/sonia.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
+import {CardConsultaShimmerEffect} from "../../componentes/CardConsultaShimmerEffect";
 
-export default function Principal({ navigation }) {
+export default function Principal({navigation}) {
 
-  return (
-    <TelaDeFundo>
-    <View style={styles.container}>
-      <InformacoesUsuario
-        nome="Olá Sônia"
-        detalhes="Mais 4 consultas hoje"
-        foto={soniaFoto}
-      />
+    const [tempo, setTempo] = useState(false);
 
-      <Text style={styles.texto}>Hoje</Text>
+    useEffect(() => {
+        setTimeout(() => {
+            setTempo(true)
+        }, 3000);
+    }, [])
 
-      <FlatList
-        data={pacientes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({ item }) =>
-        <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
-          <CardConsulta {...item} />
-        </TouchableOpacity>
-        }
-        showsVerticalScrollIndicator={false}
-      />
-    </View>
-    </TelaDeFundo>
-  );
+    return (
+        <TelaDeFundo>
+            <View style={styles.container}>
+                <InformacoesUsuario
+                    nome="Olá Sônia"
+                    detalhes="Mais 4 consultas hoje"
+                    foto={soniaFoto}
+                />
+
+                <Text style={styles.texto}>Hoje</Text>
+
+                {tempo ?
+                    <FlatList
+                        data={pacientes}
+                        keyExtractor={item => String(item.id)}
+                        renderItem={({item}) =>
+                            <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
+                                <CardConsulta {...item} />
+                            </TouchableOpacity>
+                        }
+                        showsVerticalScrollIndicator={false}
+                    />
+
+                    :
+                    <>
+                        <CardConsultaShimmerEffect/>
+                        <CardConsultaShimmerEffect/>
+                        <CardConsultaShimmerEffect/>
+                        <CardConsultaShimmerEffect/>
+                    </>
+                }
+
+
+            </View>
+        </TelaDeFundo>
+    );
 }
