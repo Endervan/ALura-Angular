eb186b0ed8561f933ec79b2b754c02d28d3efe2a
Edvan.ximes <endvan@gmail.com>
2023-07-05 16:31:07 -0300

    Entendendo o SCSS

--- jornada-milhas/src/app/app.module.ts
+++ jornada-milhas/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './shared/header/header.component';
-import { BannerComponent } from './shared/banner/banner.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { CardComponent } from './shared/card/card.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './shared/header/header.component';
+import {BannerComponent} from './shared/banner/banner.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {CardComponent} from './shared/card/card.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { CardComponent } from './shared/card/card.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
