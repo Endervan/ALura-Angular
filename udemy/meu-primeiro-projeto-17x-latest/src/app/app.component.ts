27e102fcc06c5ec9271ad505c7e752df6e673e65
Ender <endvan@gmail.com>
2024-06-28 19:37:17 -0300

    operacoes usando interpolacion

--- udemy/meu-primeiro-projeto-17x-latest/src/app/app.component.ts
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/app.component.ts
@@ -2,17 +2,19 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {NewComponent} from "./components/new-component/new-component.component";
+import {TemplateBindingComponent} from "./template/template-binding/template-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NewComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
   template: `
     <!--  <router-outlet></router-outlet>-->
     <h1>Curso de ANGULAr</h1>
     <p>Components</p>
+    <app-template-binding/>
     <div class="theme-red">
-    <app-new-component/>
+      <app-new-component/>
     </div>
   `,
 })
---
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/template/template-binding/template-binding.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-template-binding',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-binding.component.html',
+  styleUrl: './template-binding.component.scss'
+})
+export class TemplateBindingComponent {
+
+  public name='Ender Ximenes';
+  public  age = 32;
+  public sum (val1:number,val2:number){
+    return val1 + val2;
+  }
+
+}
