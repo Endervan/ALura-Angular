d6f66b4f8ffee1c97b14d7adcf7effdbb353ce68
Edvan.ximes <endvan@gmail.com>
2024-06-28 20:46:08 -0300

    event bindings

--- udemy/meu-primeiro-projeto-17x-latest/src/app/template/template-binding/template-binding.component.ts
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/template/template-binding/template-binding.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-template-binding',
@@ -10,13 +11,32 @@ import {CommonModule} from "@angular/common";
 })
 export class TemplateBindingComponent {
 
-  public name='Ender Ximenes';
-  public  age = 32;
-  public sum (val1:number,val2:number){
+  public name = 'Ender Ximenes';
+  public age = 32;
+  public isDisabled = true;
+  isTextDecoration = this.age >= 32 ? 'underline' : 'none';
+
+  public sum(val1: number, val2: number) {
     return val1 + val2;
   }
 
-  public isDisabled = true;
-  isTextDecoration = this.age >= 32  ? 'underline' : 'none';
+  sumEvent() {
+    return this.age++
+  }
 
+  subEvent() {
+    return this.age--
+  }
+
+
+ public onKeyDown(event: Event) {
+     return console.log(event)
+  }
+
+  public onMouseEvent(event:MouseEvent){
+    return console.log({
+      clientX:event.clientX,
+      clienteY:event.clientY
+    })
+  }
 }
