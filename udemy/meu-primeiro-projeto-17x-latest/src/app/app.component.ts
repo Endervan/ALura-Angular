d5ee161af6de396a8c0b25b54a59adbf5f2b0341
Ender <endvan@gmail.com>
2024-06-28 18:56:11 -0300

    criando component e usando flag nova

--- udemy/meu-primeiro-projeto-17x-latest/src/app/app.component.ts
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
+import {NewComponent} from "./components/new-component/new-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NewComponent],
   template: `
     <!--  <router-outlet></router-outlet>-->
     <h1>Curso de ANGULAr</h1>
     <p>Components</p>
+    <app-new-component/>
   `,
 })
 export class AppComponent {
---
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/components/new-component/new-component.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-new-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-component.component.html',
+  styleUrl: './new-component.component.scss'
+})
+export class NewComponent{
+
+}
