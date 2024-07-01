bf161ba1239fd31e0c9da9282a2c11b8f0d3c056
Edvan.ximes <endvan@gmail.com>
2024-07-01 19:36:44 -0300

    control flow switch case

--- udemy/meu-primeiro-projeto-17x-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,11 +1,11 @@
 import {Component} from '@angular/core';
-import {AsyncPipe, NgFor, NgIf} from "@angular/common";
+import {AsyncPipe, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
 import {delay, Observable, of} from "rxjs";
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [AsyncPipe, NgIf, NgFor],// Forma nova. Forma antiga so chamar CommoModule ja contem tudo
+  imports: [AsyncPipe, NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],// Forma nova. Forma antiga so chamar CommoModule ja contem tudo
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
@@ -18,11 +18,10 @@ export class TemplateControlFlowComponent {
     'item 3',
     'item 4',
   ]).pipe(delay(3000));
-
-
-  protected itens:Array<{name:string}> = [{name: 'Ender Scopel'}]
+  switchCondicion: string = 'Afff';
+  protected itens: Array<{ name: string }> = [{name: 'Ender Scopel'}]
 
   addNewName(value: string) {
-    return this.itens.push({name:value})
+    return this.itens.push({name: value})
   }
 }
