2711249ea38f4ff0a737d8565be09a2c7b23ed3d
Edvan.ximes <endvan@gmail.com>
2024-07-02 19:52:27 -0300

    Deferrable Views - when

--- udemy/meu-primeiro-projeto-17x-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
+++ udemy/meu-primeiro-projeto-17x-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import {NewComponent} from "../../new-component/new-component.component";
+import {delay, Observable, of} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-template-deferrable-views',
   standalone: true,
   imports: [
-    NewComponent
+    NewComponent,
+    AsyncPipe
   ],
   templateUrl: './template-deferrable-views.component.html',
   styleUrl: './template-deferrable-views.component.scss'
 })
 export class TemplateDeferrableViewsComponent {
+
+  public isTrue = false;
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+  ]).pipe(delay(3000));
 
 }
