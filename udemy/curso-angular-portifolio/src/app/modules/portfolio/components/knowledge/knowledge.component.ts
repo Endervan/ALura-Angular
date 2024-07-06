94a0a21767cfefe002483553e5cf4e672cf854e5
Edvan.ximes <endvan@gmail.com>
2024-07-05 22:30:46 -0300

    svg

---
+++ udemy/curso-angular-portifolio/src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,39 @@
+import {Component, signal} from '@angular/core';
+
+// interface de svg
+import {IKnowledge} from "../../interface/IKnowledge";
+
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de conhecimento de html5',
+    },
+    {
+      src: 'assets/icons/knowledge/css3.svg',
+      alt: 'Ícone de conhecimento de css3',
+    },
+    {
+      src: 'assets/icons/knowledge/javascript.svg',
+      alt: 'Ícone de conhecimento de javascript',
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de conhecimento de angular',
+    },
+    {
+      src: 'assets/icons/knowledge/nodejs.svg',
+      alt: 'Ícone de conhecimento de nodejs',
+    },
+  ]);
+
+}
--- udemy/curso-angular-portifolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ udemy/curso-angular-portifolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
+import {KnowledgeComponent} from "../../components/knowledge/knowledge.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, KnowledgeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
