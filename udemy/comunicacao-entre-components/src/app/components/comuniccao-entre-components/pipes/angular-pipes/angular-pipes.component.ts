49bb4cb7e497016c086274cb98b4b78d46f6669d
Edvan.ximes <endvan@gmail.com>
2024-07-20 16:05:36 -0300

    pipes pt 2 and usando providers para servico BR

--- udemy/comunicacao-entre-components/src/app/components/comuniccao-entre-components/pipes/angular-pipes/angular-pipes.component.ts
+++ udemy/comunicacao-entre-components/src/app/components/comuniccao-entre-components/pipes/angular-pipes/angular-pipes.component.ts
@@ -1,13 +1,22 @@
-import {Component, signal} from '@angular/core';
-import {AsyncPipe, DatePipe, JsonPipe, LowerCasePipe, UpperCasePipe} from "@angular/common";
+import {Component, LOCALE_ID, signal} from '@angular/core';
+import {AsyncPipe, CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, PercentPipe, registerLocaleData, UpperCasePipe} from "@angular/common";
 import {Observable, of} from "rxjs";
+
+
+//corrigindo numbes , percent e moeda para formato BR
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+//corrigindo numbes , percent e moeda para formato BR
+
 
 @Component({
   selector: 'app-angular-pipes',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, LowerCasePipe, JsonPipe, AsyncPipe],
+  imports: [DatePipe, UpperCasePipe, LowerCasePipe, JsonPipe, AsyncPipe, CurrencyPipe, DecimalPipe, PercentPipe],
   templateUrl: './angular-pipes.component.html',
-  styleUrl: './angular-pipes.component.scss'
+  styleUrl: './angular-pipes.component.scss',
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}] //corrigindo numbes , percent e moeda para formato BR
+
 })
 export class AngularPipesComponent {
   public date = signal(new Date());
