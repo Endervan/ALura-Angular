ef5dbc0c3262de917832378cfc9d222cb45cdb1e
Ender <endvan@gmail.com>
2024-08-09 19:55:59 -0300

    select drives

--- udemy/comunicacao-entre-components/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ udemy/comunicacao-entre-components/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    JsonPipe
   ],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss'
 })
 export class TemplateDrivenFormsComponent {
+
+  public listComidas = signal<Array<{ comida: string, preco: string }>>([
+    {
+      comida: 'x-salada', preco: '$RS 10'
+    }, {
+      comida: 'x-BACON', preco: '$RS 15'
+    }, {
+      comida: 'x-tudo', preco: '$RS 20'
+    }
+  ])
 
 }
