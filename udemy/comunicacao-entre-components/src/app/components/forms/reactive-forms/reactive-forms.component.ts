08d896caeafa0020092bb54a52848e8e84a737c5
Ender <endvan@gmail.com>
2024-08-26 21:36:43 -0300

    validators

--- udemy/comunicacao-entre-components/src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ udemy/comunicacao-entre-components/src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,28 +1,47 @@
-import {Component} from '@angular/core';
-import {FormArray, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {Component, inject} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    JsonPipe
   ],
   templateUrl: './reactive-forms.component.html',
   styleUrl: './reactive-forms.component.scss'
 })
 export class ReactiveFormsComponent {
-  public profileForm = new FormGroup({
-    name: new FormControl('Ender react'),
-    myStacks: new FormGroup({
-      front: new FormControl('Angular'),
-      back: new FormControl('NodeJs')
+
+  // FormBuilder no inject #=>metodo private
+  #fb = inject(FormBuilder);
+
+  // FormBuilder no constructor
+  // constructor(private _fb: FormBuilder) {
+  // }
+
+  public profileForm = this.#fb.group({
+    // name: ['',[Validators.minLength(2),Validators.maxLength(5)]],
+    // name: ['',Validators.required],
+    name: ['',Validators.email],
+    myStacks: this.#fb.group({
+      front: ['Angular'],
+      back: ['NodeJs']
     }),
-    myFavoriteFoods : new FormArray([
-      new FormControl('x- tudo'),
-    ])
+    myFavoriteFoods: this.#fb.array([['x- tudo']])
   });
 
 
+  // public profileForm = new FormGroup({
+  // name: new FormControl('Ender react'),
+  // myStacks: new FormGroup({
+  //   front: new FormControl('Angular'),
+  //   back: new FormControl('NodeJs')
+  // }),
+  // myFavoriteFoods : new FormArray([
+  //   new FormControl('x- tudo'),
+  // ])
 
 
   update() {
@@ -35,9 +54,11 @@ export class ReactiveFormsComponent {
     })
   }
 
-  public addMyFavoriteFoods(newFood:string){
-    const  myFavoriteFoods = this.profileForm.get('myFavoriteFoods') as FormArray;
-    const  addNewFood = new FormControl(newFood);
+  public addMyFavoriteFoods(newFood: string) {
+    const myFavoriteFoods = this.profileForm.get('myFavoriteFoods') as FormArray;
+    const addNewFood = new FormControl(newFood);
     myFavoriteFoods.push(addNewFood)
   }
+
+
 }
