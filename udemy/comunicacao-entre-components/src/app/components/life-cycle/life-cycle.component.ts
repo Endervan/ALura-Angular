df77338072787ec0759ee00fa62558278e98f178
Ender <endvan@gmail.com>
2024-08-29 19:56:14 -0300

    ciclo de vida - DoCheck

--- udemy/comunicacao-entre-components/src/app/components/life-cycle/life-cycle.component.ts
+++ udemy/comunicacao-entre-components/src/app/components/life-cycle/life-cycle.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, DoCheck, Input, OnChanges, OnInit, signal, SimpleChanges} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
+
+
 
 @Component({
   selector: 'app-life-cycle',
@@ -8,19 +10,31 @@ import {FormBuilder} from "@angular/forms";
   templateUrl: './life-cycle.component.html',
   styleUrl: './life-cycle.component.scss'
 })
-export class LifeCycleComponent implements OnChanges{
-  @Input() public myNumber = 0
+export class LifeCycleComponent implements OnChanges,OnInit,DoCheck{
+  @Input() public myNumber = 0;
+
 
   // construtor ou inicializador
   constructor(private  fb:FormBuilder){
   }
 
-  // changesdetections vem primeiro no ciclo de vida antes ngOnInit e detecta mudanças
+  // 1 - changesdetections vem primeiro no ciclo de vida antes ngOnInit e detecta mudanças
   ngOnChanges(changes: SimpleChanges): void {
     console.log('ngOnChanges',changes);
-    if (changes['myNumber'].previousValue === 2){
-      alert('deu bom qnd chegou no 2')
-    }
+    // if (changes['myNumber'].previousValue === 2){
+    //   alert('deu bom qnd chegou no 2')
+    // }
+  }
+
+  // 2 - ciclo de vida
+  public myText =  signal('jose iniciando ante do click');
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+
+  // executando antes e claro depois que tei alguma alteração manualmente come um click
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
   }
 
 }
