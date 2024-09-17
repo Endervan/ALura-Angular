a4516164603780cb05b4f37571c2d84932892c41
Ender_2024 <endvan@gmail.com>
2024-09-16 21:51:07 -0300

    rotas : coringa

--- udemy/comunicacao-entre-components/src/app/app.routes.ts
+++ udemy/comunicacao-entre-components/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {SobreComponent} from "./pages/sobre/sobre.component";
 import {ServicosPrestadosComponent} from "./pages/servicos-prestados/servicos-prestados.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
 export const routes: Routes = [
   {
@@ -16,6 +17,10 @@ export const routes: Routes = [
     path:'servicos/:id',
     title:'Serviços Prestados',
     component:ServicosPrestadosComponent
-  },
+  },{
+    path:'**',
+    title:'Pagina Não encontrada',
+    component:NotFoundComponent
+  }
 
 ];
