0212d1aeb25724132593c32ed64039a9e252305c
Ender_2024 <endvan@gmail.com>
2024-09-20 20:06:24 -0300

    rotas : queryParamMap

--- udemy/comunicacao-entre-components/src/app/pages/servicos-prestados/servicos-prestados.component.ts
+++ udemy/comunicacao-entre-components/src/app/pages/servicos-prestados/servicos-prestados.component.ts
@@ -1,6 +1,5 @@
 import {ChangeDetectionStrategy, Component, inject, Input, OnInit, signal} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-servicos-prestados',
@@ -12,10 +11,10 @@ import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 })
 export default class ServicosPrestadosComponent implements OnInit {
 
-  #route = inject(ActivatedRoute);
-
   // forma mais nova
   public getId = signal<null | string>(null)
+  #route = inject(ActivatedRoute); // serve para passar params
+  #router = inject(Router);
 
   @Input() set id(id: string) {
     console.log("forma nova pega id ", id)
@@ -28,17 +27,23 @@ export default class ServicosPrestadosComponent implements OnInit {
     // outra forma busca params pela url
     this.#route.params.subscribe(params => console.log(params['id']));
 
-    console.log("pegando do qurery params ",this.#route.snapshot.queryParams['name'])
-    console.log("pegando do qurery params ",this.#route.snapshot.queryParams['age'])
+    console.log("pegando do qurery params ", this.#route.snapshot.queryParams['name'])
+    console.log("pegando do qurery params ", this.#route.snapshot.queryParams['age'])
 
 
-    this.#route.queryParamMap.subscribe((res)=>console.log("pegando direto peloqueryParamMap ",res.get('name')))
+    this.#route.queryParamMap.subscribe((res) => console.log("pegando direto peloqueryParamMap ", res.get('name')))
     this.#route.queryParamMap.subscribe({
       next: (next) => {
-        console.log("pegando direto pelo queryParamMap pelo next",next.get('name'))
-        console.log("pegando direto pelo queryParamMap pelo next",next.get('age'))
+        console.log("pegando direto pelo queryParamMap pelo next", next.get('name'))
+        console.log("pegando direto pelo queryParamMap pelo next", next.get('age'))
       },
     })
+
+    setTimeout(()=>{
+      // serve para fazer redirecionamento
+      this.#router.navigate(['/cursos'])
+    },3000)
+
 
 
   }
