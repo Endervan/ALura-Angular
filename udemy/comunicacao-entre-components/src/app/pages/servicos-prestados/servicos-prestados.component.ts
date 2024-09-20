bf8dae34c7900c176a79ad278fd1cd4c7e76b765
Ender_2024 <endvan@gmail.com>
2024-09-20 19:59:50 -0300

    rotas : queryParamMap

--- udemy/comunicacao-entre-components/src/app/pages/servicos-prestados/servicos-prestados.component.ts
+++ udemy/comunicacao-entre-components/src/app/pages/servicos-prestados/servicos-prestados.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, inject, Input, OnInit, signal} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-servicos-prestados',
@@ -26,6 +27,18 @@ export default class ServicosPrestadosComponent implements OnInit {
     console.log(this.#route.snapshot.params['id'])
     // outra forma busca params pela url
     this.#route.params.subscribe(params => console.log(params['id']));
+
+    console.log("pegando do qurery params ",this.#route.snapshot.queryParams['name'])
+    console.log("pegando do qurery params ",this.#route.snapshot.queryParams['age'])
+
+
+    this.#route.queryParamMap.subscribe((res)=>console.log("pegando direto peloqueryParamMap ",res.get('name')))
+    this.#route.queryParamMap.subscribe({
+      next: (next) => {
+        console.log("pegando direto pelo queryParamMap pelo next",next.get('name'))
+        console.log("pegando direto pelo queryParamMap pelo next",next.get('age'))
+      },
+    })
 
 
   }
