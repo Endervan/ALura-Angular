4be1bbd6a86dd7b8524ae0fe3ea4a4b7d7e87d22
Ender_2024 <endvan@gmail.com>
2024-09-30 21:05:57 -0300

    animations:transition + animate

--- udemy/comunicacao-entre-components/src/assets/i18n/components/animations/animations.component.ts
+++ udemy/comunicacao-entre-components/src/assets/i18n/components/animations/animations.component.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
-import {state, style, trigger} from "@angular/animations";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-animations',
@@ -12,11 +12,14 @@ import {state, style, trigger} from "@angular/animations";
     trigger('move-ball', [
       state('move-left', style({transform: 'scale(1) translateX(0) rotate(0deg)'})),
       state('move-right', style({transform: 'scale(0.7) translateX(300px) rotate(360deg)'})),
+      transition('move-left <=> move-right', animate('1s ease-out')), // <=> bidimensional (teria seta signal),  DE => Para ou vise-versa
+      transition('* => move-right', animate('1s 2s')), // * => move-right sem valor inicial  no signal
+      transition('* => move-left', animate('1s')), //  * => move-left usa sem valor inicial  no signal
     ]) // pai de todos
   ]
 })
-export class AnimationsComponent {
 
-  public moveIn = signal('')
+export class AnimationsComponent {
+  public moveIn = signal('move-left')
 
 }
