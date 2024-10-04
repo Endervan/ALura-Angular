cd3355de34c91e78e94fd72d45e46dceb12a47eb
Ender <endvan@gmail.com>
2024-10-03 22:22:59 -0300

    projeto angular legado 13x

--- udemy/meu-primeiro-projeto-13x-latest/src/app/app.component.spec.ts
+++ udemy/meu-primeiro-projeto-13x-latest/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'meu-primeiro-projetoV13' title`, () => {
+  it(`should have as title 'meu-primeiro-projeto-13x-latest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meu-primeiro-projetoV13');
+    expect(app.title).toEqual('meu-primeiro-projeto-13x-latest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, meu-primeiro-projetoV13');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('meu-primeiro-projeto-13x-latest app is running!');
   });
 });
