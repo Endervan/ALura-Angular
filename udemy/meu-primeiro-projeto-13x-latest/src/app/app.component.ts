890ec6d5f7481686b97c5dbe9e6f57808966b568
Ender_2024 <endvan@gmail.com>
2024-11-03 16:28:29 -0300

    inicio angular last 13x ate 15x

--- udemy/meu-primeiro-projeto-13x-latest/src/app/app.component.ts
+++ udemy/meu-primeiro-projeto-13x-latest/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `teste`,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-13x-latest';
