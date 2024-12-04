d4acbddb461d555b34d35e88fbc19cf93f4c8410
Ender_2024 <endvan@gmail.com>
2024-12-04 20:25:41 -0300

    list pokemon

---
+++ udemy/pokedex-14x-latest/src/app/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// module routas
+import {RoutingModule} from "./routing.module";
+
+// pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import {SharedModule} from "../shared/shared.module";
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
+  exports: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
